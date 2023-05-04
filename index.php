41f869d61ca5fe666f83136365ef00cd7d4ed2f1
syifalayla <125670543+syifalayla@users.noreply.github.com>
2023-05-04 12:31:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Biodata</title>
+
+    <link rel="stylesheet" href="style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+      integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+  </head>
+  <body>
+    <header>
+    <header class="navbar-container">
+      <div class="gambar">
+        <img src="gundar.jpeg" alt="logo" />
+      </div>
+      <nav class="nav-list">
+        <ul>
+          <li><a href="#">TTL</a></li>
+          <li><a href="#">Kisah Syifa</a></li>
+          <li><a href="#">Domisili</a></li>
+          <li><a href="#">Kontak</a></li>
+        </ul>
+      </nav>
+    </header>
+    
+    <main>
+      <article>
+      <div class="content">
+        <div class="content-description">
+          <h1 class="title">Syifa Layla</h1>
+          <p>
+            A student majoring in information system. I am data driven and
+            skilled in programming language. With strong curiosity. I like to
+            learn something new in my life
+          </p>
+
+          <button>Next</button>
+        </div>
+      </article>
+        <aside>
+          <div class="content-description-biodata">
+            <div class="in-biodata">
+              <img src="syifa.jpeg" alt="biodata-picture" />
+              <table>
+                <tr>
+                  <th>Name :</th>
+                  <td>Syifa Layla</td>
+                </tr>
+                <tr>
+                  <th>Date of Birth :</th>
+                  <td>15 October 2001</td>
+                </tr>
+                <tr>
+                  <th>Domicile :</th>
+                  <td>Jakarta</td>
+                </tr>
+                <tr>
+                  <th>Student id :</th>
+                  <td>C172DSY2213</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </aside>
+      </div>
+
+      <div class="social-media">
+        <ul>
+          <li>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+          </li>
+        </ul>
+      </div>
+    </main>
+  </body>
+</html>
